1369ea889c45b824dbf82561144d4ccd4ae1c8d6
Kevin Stadler <kevin.stadler@ed.ac.uk>
2020-04-12 19:24:28 +0800

    Updates

---
+++ precache-manifest.494a293c81333974cc13c918b2815465.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "585e2d5386c4860a93d422265429924c",
+    "url": "/alongthesameline/index.html"
+  },
+  {
+    "revision": "fd1cbff28e7b2c619959",
+    "url": "/alongthesameline/static/css/2.db529fa1.chunk.css"
+  },
+  {
+    "revision": "e8b2e88d0e9fa38d247e",
+    "url": "/alongthesameline/static/css/main.5d146d36.chunk.css"
+  },
+  {
+    "revision": "fd1cbff28e7b2c619959",
+    "url": "/alongthesameline/static/js/2.df162e1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/alongthesameline/static/js/2.df162e1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8b2e88d0e9fa38d247e",
+    "url": "/alongthesameline/static/js/main.8f1e041c.chunk.js"
+  },
+  {
+    "revision": "ba813a91ff3c95c1520b",
+    "url": "/alongthesameline/static/js/runtime-main.0eb38a6c.js"
+  }
+]);
